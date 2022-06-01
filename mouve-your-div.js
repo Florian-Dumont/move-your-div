eb22e0d679e3b8d43f943e1cd58c333fd996c022
Florian <106590792+Florian-Dumont@users.noreply.github.com>
2022-06-01 09:29:36 +0200

    Update mouve-your-div.js

--- mouve-your-div.js
+++ mouve-your-div.js
@@ -1,1 +1,41 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('#moveDiv');
+    const div = document.querySelector('.red');
+    const modal = document.querySelector('.modal');
+    let clickCount = 0;
+
+    btn.addEventListener('click', function () {
+        if (clickCount > 3) {
+            clickCount = 0;
+        }
+        clickCount++;
+        moveDiv(div, modal, clickCount);
+    });
+
+    document.getElementById('hideModal').addEventListener('click', () => {
+        modal.classList.remove('active');
+        document.body.classList.remove('modal-active');
+    });
+});
+
+function moveDiv(div, modal, clickCount) {
+    switch (clickCount) {
+        case 1:
+            modal.classList.remove('active');
+            document.body.classList.remove('modal-active');
+            div.style.marginLeft = '90%';
+            break;
+        case 2:
+            div.style.marginTop = '200px';
+            break;
+        case 3:
+            div.style.marginLeft = '0';
+            break;
+        default:
+            div.style.marginTop = '0';
+            modal.classList.add('active');
+            document.body.classList.add('modal-active');
+            break;
+    }
+}
